e90abcb867eccbb514fdae96503524956e3d096a
Max Lindmark <max.lindmark@slu.se>
2023-02-21 12:35:38 +0100

    add functions

--- R/functions/map-plot.R
+++ R/functions/map-plot.R
@@ -1,7 +1,9 @@
 library(sf)
 
+sf::sf_use_s2(FALSE)
+
 # Specify map ranges
-ymin = 52; ymax = 58; xmin = 10; xmax = 24
+ymin = 52; ymax = 60.5; xmin = 10; xmax = 24
 
 map_data <- rnaturalearth::ne_countries(
   scale = "medium",
@@ -19,32 +21,34 @@ swe_coast_proj <- sf::st_transform(swe_coast, crs = utm_zone33)
 
 # Define plotting theme for main plot
 theme_plot <- function(base_size = 11, base_family = "") {
-  theme_light(base_size = 11, base_family = "") +
+  theme_light(base_size = base_size, base_family = "") +
     theme(
-      axis.text = element_text(color = "grey5"),
       legend.position = "bottom",
       legend.key.height = unit(0.2, "cm"),
       legend.margin = margin(0, 0, 0, 0),
       legend.box.margin = margin(-5, -5, -5, -5),
+      strip.text = element_text(size = 9, colour = 'gray10', margin = margin(b = 1, t = 1)),
       strip.background = element_rect(fill = "grey95"),
-      strip.text = element_text(color = "grey10"),
-      strip.text.x = element_text(margin = margin(b = 2, t = 2), color = "grey10", size = 10),
-      panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-      panel.background = element_blank()
-      #, axis.line = element_line(colour = "grey20"),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank()
     )
 }
 
 # Define plotting theme for facet_wrap map with years
-theme_facet_map <- function(base_size = 10, base_family = "") {
-  theme_light(base_size = 10, base_family = "") +
+theme_facet_map <- function(base_size = 11, base_family = "") {
+  theme_light(base_size = base_size, base_family = "") +
     theme(
       axis.text.x = element_text(angle = 90),
-      axis.text = element_text(size = 6),
+      axis.text = element_text(size = 7),
+      strip.text = element_text(size = 8, colour = 'gray10', margin = margin(b = 1, t = 1)),
       strip.background = element_rect(fill = "gray95"),
-      strip.text = element_text(margin = margin(b = 2, t = 2), color = "grey10", size = 9), 
-      legend.position = c(0.7, 0.02),
-      legend.direction = "horizontal"
+      legend.direction = "horizontal",
+      legend.margin = margin(1, 1, 1, 1),
+      legend.box.margin = margin(0, 0, 0, 0),
+      legend.key.height = unit(0.4, "line"),
+      legend.key.width = unit(2, "line"),
+      legend.spacing.x = unit(0.1, 'cm'),
+      legend.position = "bottom",
     )
 }
 
@@ -54,7 +58,7 @@ xmax2 <- 916000
 xrange <- xmax2 - xmin2
 
 ymin2 <- 5980000
-ymax2 <- 6420000
+ymax2 <- 6450000
 yrange <- ymax2 - ymin2
 
 plot_map <- 
@@ -64,7 +68,32 @@ plot_map <-
   labs(x = "Longitude", y = "Latitude") +
   geom_sf(size = 0.3) + 
   theme_plot() +
+  guides(colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
+         fill = guide_colorbar(title.position = "top", title.hjust = 0.5)) +
   NULL
+
+# This is for the diet data
+xmin3 <- 303000
+xmax3 <- 916000
+xrange <- xmax2 - xmin3
+
+ymin3 <- 5980000
+ymax3 <- 6580000
+yrange <- ymax3 - ymin3
+
+plot_map_d <- 
+  ggplot(swe_coast_proj) + 
+  xlim(xmin3*1.5, xmax3*0.9) +
+  ylim(ymin3*1.025, ymax3*0.98268) + 
+  labs(x = "Longitude", y = "Latitude") +
+  geom_sf(size = 0.3) + 
+  theme_plot() +
+  guides(colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
+         fill = guide_colorbar(title.position = "top", title.hjust = 0.5)) +
+  NULL
+
+# small_cod_stomach <- read_csv("data/clean/small_cod_stomach.csv") 
+# plot_map_d + geom_point(data = small_cod_stomach, aes(X*1000, Y*1000))
 
 plot_map_fc <- 
   ggplot(swe_coast_proj) + 
@@ -73,8 +102,9 @@ plot_map_fc <-
   labs(x = "Longitude", y = "Latitude") +
   geom_sf(size = 0.3) + 
   theme_facet_map() +
+  guides(colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
+         fill = guide_colorbar(title.position = "top", title.hjust = 0.5)) +
   NULL
-
 
 plot_map_labels <- 
   plot_map + 
@@ -96,3 +126,4 @@ plot_map_labels_fc <-
   annotate("text", label = "Lithuania", x = xmin2 + 1*xrange, y = ymin2 + 0.43*yrange, color = "black", size = 1.9, angle = 75) +
   annotate("text", label = "Latvia", x = xmin2 + 0.99*xrange, y = ymin2 + 0.65*yrange, color = "black", size = 1.9, angle = 75)
 
+# Diet map plot here!
