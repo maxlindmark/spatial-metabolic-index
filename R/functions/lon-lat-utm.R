04a36b0964046fd6ca3ac525e299a2c15b1aedd0
Max Lindmark <max.lindmark@slu.se>
2023-09-07 08:13:02 +0200

    clean the repo

--- R/functions/lon-lat-utm.R
+++
@@ -1,8 +0,0 @@
-# Lon lat to UTM function
-LongLatToUTM <- function(x, y, zone){
-  xy <- data.frame(ID = 1:length(x), X = x, Y = y)
-  coordinates(xy) <- c("X", "Y")
-  proj4string(xy) <- CRS("+proj=longlat +datum=WGS84")  ## for example
-  res <- spTransform(xy, CRS(paste("+proj=utm +zone=",zone," ellps=WGS84",sep='')))
-  return(as.data.frame(res))
-}
